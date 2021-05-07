2b3d987661e0069070f81eb5edb77e7280d9fecb
Jan M Michalski <jan.m.michalski@intel.com>
2021-05-07 12:22:43 +0100

    XXX: rpma_ep_next_conn_req using semaphores

--- tests/multithreaded/ep/rpma_ep_next_conn_req.c
+++ tests/multithreaded/ep/rpma_ep_next_conn_req.c
@@ -5,14 +5,14 @@
  * rpma_ep_next_conn_req.c -- rpma_ep_next_conn_req multithreaded test
  */
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
+
 #include <librpma.h>
 
 #include "mtt.h"
-
-#define CHILD_PROCESS_DELAY_US		1000
 
 struct prestate {
 	char *addr;
@@ -20,6 +20,9 @@ struct prestate {
 	struct ibv_context *dev;
 	struct rpma_peer *peer;
 	struct rpma_ep *ep;
+
+	sem_t *ep_ready_sem;
+	sem_t *conn_reqs_ready_sem;
 };
 
 struct state {
@@ -30,6 +33,9 @@ struct child_prestate {
 	char *addr;
 	unsigned port;
 	unsigned threads_num;
+
+	sem_t *ep_ready_sem;
+	sem_t *conn_reqs_ready_sem;
 };
 
 /*
@@ -53,6 +59,9 @@ prestate_init(void *prestate, struct mtt_result *tr)
 	MTT_PORT_SET(pr->port, 0);
 	if ((ret = rpma_ep_listen(pr->peer, pr->addr, MTT_PORT_STR, &pr->ep)))
 		MTT_RPMA_ERR(tr, "rpma_ep_listen", ret);
+
+	sem_post(pr->ep_ready_sem);
+	sem_wait(pr->conn_reqs_ready_sem);
 }
 
 /*
@@ -62,7 +71,6 @@ void
 init(unsigned id, void *prestate, void **state_ptr,
 		struct mtt_result *tr)
 {
-	struct prestate *pr = (struct prestate *)prestate;
 	struct state *st = (struct state *)calloc(1, sizeof(struct state));
 	if (!st) {
 		MTT_ERR(tr, "calloc", errno);
@@ -130,10 +138,11 @@ child_process(void *prestate)
 	struct ibv_context *dev = NULL;
 	struct rpma_peer *peer = NULL;
 	struct rpma_conn_req *req = NULL;
-	struct rpma_conn *conn = NULL;
+	struct rpma_conn **conns;
+	enum rpma_conn_event event;
 	int ret = 0;
 
-	usleep(CHILD_PROCESS_DELAY_US);
+	conns = calloc(pr->threads_num, sizeof(struct rpma_conn *));
 
 	/* obtain an IBV context for a remote IP address */
 	if ((ret = rpma_utils_get_ibv_context(pr->addr,
@@ -144,28 +153,39 @@ child_process(void *prestate)
 	if ((ret = rpma_peer_new(dev, &peer)))
 		return ret;
 
+	sem_wait(pr->ep_ready_sem);
+
 	MTT_PORT_INIT;
 	MTT_PORT_SET(pr->port, 0);
-	for (int counts = 0; counts < pr->threads_num; counts++) {
+	for (unsigned i = 0; i < pr->threads_num; i++) {
 		/* create a connection request */
 		if ((ret = rpma_conn_req_new(peer, pr->addr,
 				MTT_PORT_STR, NULL, &req)))
 			goto err_peer_delete;
 
-		if ((ret = rpma_conn_req_connect(&req, NULL, &conn))) {
+		if ((ret = rpma_conn_req_connect(&req, NULL, &conns[i]))) {
 			if (req)
 				(void) rpma_conn_req_delete(&req);
 			goto err_peer_delete;
 		}
+	}
 
-		usleep(CHILD_PROCESS_DELAY_US);
+	ret = sem_post(pr->conn_reqs_ready_sem);
+	if (ret) {
+		ret = errno;
+		goto err_peer_delete;
+	}
+
+	for (unsigned i = 0; i < pr->threads_num; i++) {
+		if ((ret = rpma_conn_next_event(conns[i], &event)))
+			goto err_conn_delete;
 
 		/* disconnect the connection */
-		if ((ret = rpma_conn_disconnect(conn)))
+		if ((ret = rpma_conn_disconnect(conns[i])))
 			goto err_conn_delete;
 
 		/* delete the connection object */
-		if ((ret = rpma_conn_delete(&conn)))
+		if ((ret = rpma_conn_delete(&conns[i])))
 			goto err_peer_delete;
 	}
 
@@ -176,7 +196,8 @@ child_process(void *prestate)
 	return 0;
 
 err_conn_delete:
-	(void) rpma_conn_delete(&conn);
+	//(void) rpma_conn_delete(&conn);
+	free(conns);
 err_peer_delete:
 	(void) rpma_peer_delete(&peer);
 err_exit:
@@ -191,9 +212,18 @@ main(int argc, char *argv[])
 	if (mtt_parse_args(argc, argv, &args))
 		return -1;
 
-	struct prestate prestate = {args.addr, args.port, NULL, NULL, NULL};
+	sem_t *ep_ready_sem = sem_open("/mt_rpma_ep_next_conn_req_ep_ready_sem", O_CREAT, 0600, 0);
+	if (ep_ready_sem == NULL) {
+		return errno;
+	}
+	sem_t *conn_reqs_ready_sem = sem_open("/mt_rpma_ep_next_conn_conn_reqs_ready_sem", O_CREAT, 0600, 0);
+	if (conn_reqs_ready_sem == NULL) {
+		return errno;
+	}
+
+	struct prestate prestate = {args.addr, args.port, NULL, NULL, NULL, ep_ready_sem, conn_reqs_ready_sem};
 	struct child_prestate cld_prestate = {args.addr, args.port,
-				args.threads_num};
+					args.threads_num, ep_ready_sem, conn_reqs_ready_sem};
 
 	struct mtt_test test = {
 			&prestate,
